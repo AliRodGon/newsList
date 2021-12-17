f39759ecbe70a4324dd64ca0c52a195d8772fe41
Alicia Rod <aliciarodri.ar@gmail.com>
2021-12-17 09:38:14 +0100

    half login made

--- newsapp/src/Components/FormLogin/FormLogin.jsx
+++ newsapp/src/Components/FormLogin/FormLogin.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
+import {userContext} from '../../Context/userContext'
+
 
 class FormLogin extends Component {
-  
+
+  static contextType = userContext
+
+  constructor(props) {
+    super(props)
+    this.name = React.createRef(); // crear la referencia
+    this.state = {
+    }
+  }
+
+handleSubmit = event => {
+  event.preventDefault();
+  const name = this.name.current.value   
+  const { user, login } = this.context
+  login(name);
+console.log(this.context.user)
+};
+
   render() {
     return <div>
-              <form>
-                <label>Hola, dinos tu nombre</label>
-                <input type="text"></input>
-                <input type="submit"></input>
-              </form>
-      </div>
+                  <h2>Hola {this.context.user}</h2>
+                  <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Nombre:  </label>
+                    <input type="text" id="name" name="name" ref={this.name}/>
+                    <input type="submit"></input>
+                  </form>
+              </div>
   }
 }
 
