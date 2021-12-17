bec6560c8567465efc693237cf6b54b5aa20651f
Alicia Rod <aliciarodri.ar@gmail.com>
2021-12-17 17:43:26 +0100

    array of5 news fetched

--- newsapp/src/Components/Card/Card.jsx
+++ newsapp/src/Components/Card/Card.jsx
@@ -1,9 +1,27 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 class Card extends Component {
-  render() {
-    return <div>Card</div>
-  }
-}
+
+// constructor(props){
+//   super(props); 
+//   this.state={
+//     value:''
+//   }
+// }
+// fetchNews=()=>{
+//   axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.state.value}&api-key=p7j0H5UQiOmNzNlm8EnUa5ZLGBsWT2oG`)
+// .then((response)=>{
+// const testing= response.data;
+// console.log(testing);
+// }
+// }
+//   render() {
+//     return <div>
+//       <h2 type="text" value={this.state.value} ></h2>
+//       <button>BORRAR</button>
+//       </div>
+//   }
+ }
 
 export default Card;
--- newsapp/src/Components/List/List.jsx
+++ newsapp/src/Components/List/List.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 class List extends Component {
-  render() {
-    return <div>NewsList</div>
-  }
-}
+  
+    constructor(props) {
+      super(props);
+      this.state = { datanews: [] }
+      // Event binding (Bindear eventos)
+      // console.log(this.state.datanews)
+    }
+    async componentDidMount(){
+      //await new Promise( resolve => setTimeout(resolve, 3000)); // Simular retardo de una carga más "lenta"
+      
+      const resp = await axios.get('https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=p7j0H5UQiOmNzNlm8EnUa5ZLGBsWT2oG');
+      // const newObject = resp.data.response.docs[0].abstract;
+      const newsarray = resp.data.response.docs
+      const newBlockofNews= newsarray.slice(0, 5).map((item) => {
+        
+        console.log(item)
+        return item;
+    });
 
+
+      
+      // const results = resp.data; // []
+      
+  
+      this.setState({
+          datanews: ([newBlockofNews]) // []
+         
+      })
+     
+    }
+    
+    render() {
+      
+      return (
+          <p> {}</p>
+      );
+    }
+  }
+  
 export default List;
